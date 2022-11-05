785999c5b2cb3dc6c1bf1326a3f28bcc1c13f0fd
yiqiqvkan <94735569+yiqiqvkan@users.noreply.github.com>
2022-11-05 14:22:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import os
 import random
 from dateutil.utils import today
 
-today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+birthday_female = os.environ['BIRTHDAY_FEMALE']
+birthday_male = os.environ['BIRTHDAY_MALE']
+key = os.environ['KEY']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -19,36 +20,69 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
+
+
+def get_cai():
+  response = requests.get(url='https://api.tianapi.com/caihongpi/index?key=' + key).json()
+  return response["newslist"][0]['content']
+
+
+def get_birthday(birthday):
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today()).days
+
+
+def get_togetherTime():
+  next = datetime.strptime(start_date, "%Y-%m-%d")
+  return (today() - next).days
+
+
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+def get_date():
+  d = datetime.today()  # 获取当前日期时间
+  week = d.isoweekday()
+  date = today().strftime("%Y-%m-%d")
+  if week == 1:
+    return date + " 星期一"
+  if week == 2:
+    return date + " 星期二"
+  if week == 3:
+    return date + " 星期三"
+  if week == 4:
+    return date + " 星期四"
+  if week == 5:
+    return date + " 星期五"
+  if week == 6:
+    return date + " 星期六"
+  if week == 7:
+    return date + " 周日"
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"date": {"value": get_date(), "color": get_random_color()},
+        "city": {"value": city, "color": get_random_color()},
+        "weather": {"value": get_weather()['weather'], "color": get_random_color()},
+        "min_temperature": {"value": str(get_weather()['low']) + "℃", "color": get_random_color()},
+        "max_temperature": {"value": str(get_weather()['high']) + "℃", "color": get_random_color()},
+        "pipi": {"value": get_cai(), "color": get_random_color()},
+        "love_day": {"value": get_togetherTime(), "color": get_random_color()},
+        "birthday1": {"value": get_birthday(birthday_female), "color": get_random_color()},
+        "birthday1": {"value": get_birthday(birthday_male), "color": get_random_color()},
+        }
 res = wm.send_template(user_id, template_id, data)
 print(res)
